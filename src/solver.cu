6c24446a9a400a68038a7beed396cd0fe4fd5c4f
Nishanta Baral <nishantabaral48@gmail.com>
2026-04-24 00:42:02 -0700

    testing Eigen on HPC

--- src/solver.cu
+++ src/solver.cu
@@ -1,9 +1,28 @@
 #include "solver.hpp"
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Eigenvalues>
+
+using namespace std;
+using namespace Eigen;
 
 Result solve(double theta, double phi) {
+    Matrix2d A;
+    A << 0,  1,
+         1, -0.1;
+
+    cout << "--- HPC Eigen Test ---" << endl;
+    cout << "Inputs -> Theta: " << theta << ", Phi: " << phi << endl;
+    cout << "Matrix A:" << endl << A << endl;
+
+    EigenSolver<Matrix2d> solver(A);
+    cout << "Eigenvalues:" << endl << solver.eigenvalues() << endl;
+    cout << "----------------------" << endl;
+
     Result r;
     r.l1 = 0.0;
     r.l2 = 0.0;
-    r.cost = 1e100;
+    r.cost = 0.0;
+
     return r;
 }
